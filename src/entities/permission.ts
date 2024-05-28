6963523141a6d1ac5efe40d2aa94449423d27824
Fábio <fabiogoncalvesk2001@gmail.com>
2024-05-28 18:42:18 +0100

    make permission entity extend base entity

--- src/entities/permission.ts
+++ src/entities/permission.ts
@@ -1,7 +1,8 @@
 import { Column, Entity } from 'typeorm';
+import { Base } from './base.entity';
 
 @Entity()
-export class Permission {
+export class Permission extends Base {
   @Column({ unique: true })
   name: string;
 
