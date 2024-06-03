d98b51dc0a41362597dfa4272306eae5edf1f71f
Fábio <fabiogoncalvesk2001@gmail.com>
2024-06-03 13:44:40 +0100

    add configuration for the jwt authentication in the project

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User } from 'src/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from 'src/entities/role.entity';
+import { Permission } from 'src/entities/permission.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.getOrThrow('SECRET'),
+        signOptions: { expiresIn: '15 days' },
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([User, Role, Permission]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+})
+export class AuthModule {}
