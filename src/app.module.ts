5f211c4ea70d7c71c9b35dc2cb4e4b47a18fb033
Fábio <fabiogoncalvesk2001@gmail.com>
2024-06-03 13:34:47 +0100

    add AuthModule as an import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
